115f72a5afe7fbeb97465617b8ad423a870bd870
jiangshan00001 <710806594@qq.com>
2021-12-14 15:32:05 +0800

    ostream error fix

--- src/misc/util/mylog.cpp
+++ src/misc/util/mylog.cpp
@@ -1,3 +1,4 @@
+
 /// 2020.7.31 将记录类拆分为2个lei
 #include <iostream>
 #include <fstream>
@@ -6,11 +7,11 @@
 namespace mylog {
 
 
-MyLog cout("INFO:");
-MyLog cerr("ERROR:");
-MyLog cdbg("DEBUG:");
-MyLog cwarn("WARN:");
-MyLog csummary("SUMMARY:");
+	MyLog cout("INFO:");
+	MyLog cerr("ERROR:");
+	MyLog cdbg("DEBUG:");
+	MyLog cwarn("WARN:");
+	MyLog csummary("SUMMARY:");
 }
 
 mylog::MyStreamBuf::MyStreamBuf()
@@ -25,34 +26,34 @@ mylog::MyStreamBuf::~MyStreamBuf()
 
 int mylog::MyStreamBuf::overflow(int c)
 {
-    m_callback->foo(c);
-    return 0;
+	m_callback->foo(c);
+	return 0;
 }
 
 
-mylog::MyLog::MyLog(std::string prefix) 
-:std::ostream()
+mylog::MyLog::MyLog(std::string prefix)
+	:std::ostream(&this->m_stream_buf)
 {
-    m_stream_buf.m_callback = this;
-    this->init(&m_stream_buf);
-    is_c_cout = 1;
-    m_is_new_line = 1;
-    m_prefix=prefix;
+	m_stream_buf.m_callback = this;
+	this->init(&m_stream_buf);
+	is_c_cout = 1;
+	m_is_new_line = 1;
+	m_prefix = prefix;
 
-    m_line_count = 0;
+	m_line_count = 0;
 
 }
 
-mylog::MyLog::MyLog() 
-:std::ostream()
+mylog::MyLog::MyLog()
+	:std::ostream(&this->m_stream_buf)
 {
-    m_stream_buf.m_callback = this;
-    this->init(&m_stream_buf);
-    is_c_cout = 1;
-    m_is_new_line = 1;
-    m_prefix="";
+	m_stream_buf.m_callback = this;
+	this->init(&m_stream_buf);
+	is_c_cout = 1;
+	m_is_new_line = 1;
+	m_prefix = "";
 
-    m_line_count = 0;
+	m_line_count = 0;
 
 }
 
@@ -65,89 +66,91 @@ mylog::MyLog::~MyLog()
 
 void mylog::MyLog::foo(char c)
 {
-    if(m_is_new_line)
-    {
-        m_is_new_line = 0;
-        if(is_c_cout) std::cout<<m_prefix;
-        if(m_log_stream.is_open()) m_log_stream<<m_prefix;
+	if (m_is_new_line)
+	{
+		m_is_new_line = 0;
+		if (is_c_cout) std::cout << m_prefix;
+		if (m_log_stream.is_open()) m_log_stream << m_prefix;
 
 
-    }
+	}
 
-    if(is_c_cout)std::cout.put(c);
-    if(m_log_stream.is_open()) m_log_stream<<c;
-    m_log_stream.flush();
+	if (is_c_cout)std::cout.put(c);
+	if (m_log_stream.is_open()) m_log_stream << c;
+	m_log_stream.flush();
 
-    if(c=='\n')
-    {
-        m_is_new_line = 1;
+	if (c == '\n')
+	{
+		m_is_new_line = 1;
 
-        m_line_count++;
-    }
+		m_line_count++;
+	}
 }
 
 int mylog::MyLog::set_log_file(std::string file_name)
 {
-    if(file_name.size()>0)
-    {
-        m_log_file_name = file_name;
-    }
+	if (file_name.size() > 0)
+	{
+		m_log_file_name = file_name;
+	}
 
-    if(m_log_stream.is_open())
-    {
-        m_log_stream.close();
-    }
-    m_log_stream.open(m_log_file_name.c_str(), std::ios_base::out| std::ios_base::app );
-    return 0;
+	if (m_log_stream.is_open())
+	{
+		m_log_stream.close();
+	}
+	m_log_stream.open(m_log_file_name.c_str(), std::ios_base::out | std::ios_base::app);
+	return 0;
 }
 
 int mylog::MyLog::close_log_file()
 {
-    if(m_log_stream.is_open())
-    {
-        m_log_stream.close();
-    }
-    return 0;
+	if (m_log_stream.is_open())
+	{
+		m_log_stream.close();
+	}
+	return 0;
 }
 
 int mylog::MyLog::set_cout_output(int is_out)
 {
-    is_c_cout = is_out;
+	is_c_cout = is_out;
+	return 0;
 }
 
 mylog::MyLog &mylog::MyLog::print_hex(std::string istr, int num_of_one_line)
 {
-    for(unsigned i=0;i<istr.size();++i)
-    {
-        unsigned char A = istr[i];
-        unsigned char AA = ((A & 0xf0)>>4);
-        unsigned char AB = ((A & 0x0F)>>0);
-            if((i!=0) &&(i%num_of_one_line==0))
-            {
-                foo('\n');
-            }
-            else
-            {
-                foo(' ');
-            }
-        foo(char_to_hex_disp(AA));
-        foo(char_to_hex_disp(AB));
-    }
-    foo('\n');
+	for (unsigned i = 0; i < istr.size(); ++i)
+	{
+		unsigned char A = istr[i];
+		unsigned char AA = ((A & 0xf0) >> 4);
+		unsigned char AB = ((A & 0x0F) >> 0);
+		if ((i != 0) && (i%num_of_one_line == 0))
+		{
+			foo('\n');
+		}
+		else
+		{
+			foo(' ');
+		}
+		foo(char_to_hex_disp(AA));
+		foo(char_to_hex_disp(AB));
+	}
+	foo('\n');
+	return *this;
 
 }
 
 unsigned char mylog::MyLog::char_to_hex_disp(unsigned char ia)
 {
-    if((ia>=0)&&
-            (ia<=9))
-    {
-        return ia+'0';
-    }
-    else if ((ia>=10)&&
-            (ia<=15))
-    {
-        return ia-10+'A';
-    }
-        return 'G';
+	if ((ia >= 0) &&
+		(ia <= 9))
+	{
+		return ia + '0';
+	}
+	else if ((ia >= 10) &&
+		(ia <= 15))
+	{
+		return ia - 10 + 'A';
+	}
+	return 'G';
 }
--- src/misc/util/mylog.h
+++ src/misc/util/mylog.h
@@ -1,3 +1,5 @@
+
+
 #ifndef MYLOG_H
 #define MYLOG_H
 
@@ -10,74 +12,75 @@ namespace mylog {
 
 
 
-class MyStreamBuf: public std::streambuf
-{
-public:
-    MyStreamBuf();
-    ~MyStreamBuf();
-protected:
+	class MyStreamBuf : public std::streambuf
+	{
+	public:
+		MyStreamBuf();
+		~MyStreamBuf();
+	protected:
 
 
-    ///Basically just subclass the ostream and the streambuf objects,
-    /// and construct the ostream with itself as the buffer.
-    /// the virtual overflow() from std::streambuf will be called for every character sent to the stream.
-    /// To fit your example i just made a foo() function and called it.
-    ///
-    virtual int overflow(int c);
-public:
-    class MyLog *m_callback;
+		///Basically just subclass the ostream and the streambuf objects,
+		/// and construct the ostream with itself as the buffer.
+		/// the virtual overflow() from std::streambuf will be called for every character sent to the stream.
+		/// To fit your example i just made a foo() function and called it.
+		///
+		virtual int overflow(int c);
+	public:
+		class MyLog *m_callback;
 
-};
+	};
 
-class MyLog :public std::ostream
-{
-public:
-    MyLog(std::string prefix);
-	MyLog();
-    ~MyLog();
-
-
-
-protected:
-    ///Basically just subclass the ostream and the streambuf objects,
-    /// and construct the ostream with itself as the buffer.
-    /// the virtual overflow() from std::streambuf will be called for every character sent to the stream.
-    /// To fit your example i just made a foo() function and called it.
-    ///
-    //virtual int overflow(int c);
-public:
-    void foo(char c);
-    int set_log_file(std::string file_name="");
-    int close_log_file();
-    int set_cout_output(int is_out);
-
-    //字符输出为hex格式。5个一行
-    MyLog& print_hex(std::string istr, int num_of_one_line=5);
-
-private:
-    std::string m_log_file_name;
-    std::ofstream m_log_stream;
-    int is_c_cout;
-private:
-    std::string m_prefix;
-    int m_is_new_line;
-    MyStreamBuf m_stream_buf;
-public:
-    int m_line_count;
-
-private:
-    unsigned char char_to_hex_disp(unsigned char ia);
-};
+	class MyLog :public std::ostream
+	{
+	public:
+		MyLog(std::string prefix);
+		MyLog();
+		~MyLog();
 
 
-extern  MyLog cout;
-extern MyLog cerr;
-extern MyLog cdbg;
-extern MyLog cwarn;
-extern MyLog csummary;
+
+	protected:
+		///Basically just subclass the ostream and the streambuf objects,
+		/// and construct the ostream with itself as the buffer.
+		/// the virtual overflow() from std::streambuf will be called for every character sent to the stream.
+		/// To fit your example i just made a foo() function and called it.
+		///
+		//virtual int overflow(int c);
+	public:
+		void foo(char c);
+		int set_log_file(std::string file_name = "");
+		int close_log_file();
+		int set_cout_output(int is_out);
+
+		//字符输出为hex格式。5个一行
+		MyLog& print_hex(std::string istr, int num_of_one_line = 5);
+
+	private:
+		std::string m_log_file_name;
+		std::ofstream m_log_stream;
+		int is_c_cout;
+	private:
+		std::string m_prefix;
+		int m_is_new_line;
+		MyStreamBuf m_stream_buf;
+	public:
+		int m_line_count;
+
+	private:
+		unsigned char char_to_hex_disp(unsigned char ia);
+	};
+
+
+	extern  MyLog cout;
+	extern MyLog cerr;
+	extern MyLog cdbg;
+	extern MyLog cwarn;
+	extern MyLog csummary;
 
 }
 
 
 
 #endif // MYLOG_H
+
